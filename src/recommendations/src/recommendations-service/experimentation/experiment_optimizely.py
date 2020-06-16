a0f4ae71709b5824fe387055a3597b1f7246c629
Yeeland Chen <yeeland.chen@optimizely.com>
2020-06-16 14:47:04 -0500

    Need to let the frontend know that the items come from an Optimizely experiemnt and the datafile revision number to make sure the frontend and backend are on the same page.

--- src/recommendations/src/recommendations-service/experimentation/experiment_optimizely.py
+++ src/recommendations/src/recommendations-service/experimentation/experiment_optimizely.py
@@ -1,8 +1,11 @@
-from . import experiment, resolvers
+import os
+
 from optimizely import optimizely
 
-# TODO: Ask James how we want the recommendations service to store the SDK Key
-optimizely_sdk = optimizely.Optimizely(sdk_key='SDK_KEY')
+from . import experiment, resolvers
+
+
+optimizely_sdk = optimizely.Optimizely(sdk_key=os.environ('OPTIMIZELY_SDK_KEY'))
 
 
 class OptimizelyFeatureTest(experiment.Experiment):
@@ -10,7 +13,6 @@ class OptimizelyFeatureTest(experiment.Experiment):
         assert user_id, "`user_id` is required"
 
         # All the kwargs that are passed to ResolverFactory.get will be stored as a JSON feature variable.
-        optimizely_sdk.is_feature_enabled(self.feature, user_id) # This sends an impression event
         resolver_init_kwargs = optimizely_sdk.get_feature_variable_json(self.feature, 'resolver_init_kwargs',
                                                                         user_id=user_id)
         resolver = resolvers.ResolverFactory.get(**resolver_init_kwargs)
@@ -20,5 +22,9 @@ class OptimizelyFeatureTest(experiment.Experiment):
                                    product_list=item_list,
                                    num_results=num_results)
 
-        # TODO: Need to track impression events.
+        config = optimizely_sdk.config_manager.get_config()
+
+        for item in items:
+            item['experiment'] = {'type': 'optimizely',
+                                  'revision_number': config.get_revision()}
         return items
